321050083ba6b7c65e657a936d38b969e22508d8
Casinopoipa DevOps <deploy@casinopoipa.com.py>
2026-07-16 11:50:15 -0300

    Forzar activacion de base de datos directa en arranque

--- casinopoipa/wsgi.py
+++ casinopoipa/wsgi.py
@@ -3,3 +3,13 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "casinopoipa.settings")
 application = get_wsgi_application()
+
+import django
+from django.core.management import call_command
+
+django.setup()
+try:
+    call_command("migrate", interactive=False)
+    print("Base de datos en la nube activada con éxito.")
+except Exception as e:
+    print("Error al migrar:", e)
