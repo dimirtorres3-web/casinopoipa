d04ead685759b258e95fd21adf7e49a118db0ec7
dimirtorres3-web <dimirtorres3@gmail.com>
2026-08-11 17:41:44 -0300

    Delete casino directory

--- casino/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CasinoConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "casino"
-    verbose_name = "Casinopoipa"
